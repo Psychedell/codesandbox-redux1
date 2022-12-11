7c09efe50914dce719be1502029dc5bdf9023ab2
Oleg Kaminskyy <100085494+Psychedell@users.noreply.github.com>
2022-12-11 21:26:16 +0200

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { createStore } from "redux";
+import { devToolsEnhancer } from "@redux-devtools/extension";
+
 // Начальное значение состояния Redux для корневого редюсера,
 // если не передать параметр preloadedState.
 const initialState = {
@@ -18,4 +20,7 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
   return state;
 };
-export const store = createStore(rootReducer);
+
+const enhancer = devToolsEnhancer();
+
+export const store = createStore(rootReducer, enhancer);
